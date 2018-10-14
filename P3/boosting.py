f0adacd6f4ad972aef11c0926f67b4bf47942cc1
takatogoto <gg5taka@gmail.com>
2018-10-14 16:29:14 -0700

    boosting

--- P3/boosting.py
+++ P3/boosting.py
@@ -7,58 +7,77 @@ from abc import abstractmethod
 
 class Boosting(Classifier):
   # Boosting from pre-defined classifiers
-	def __init__(self, clfs: Set[Classifier], T=0):
-		self.clfs = clfs      # set of weak classifiers to be considered
-		self.num_clf = len(clfs)
-		if T < 1:
-			self.T = self.num_clf
-		else:
-			self.T = T
-	
-		self.clfs_picked = [] # list of classifiers h_t for t=0,...,T-1
-		self.betas = []       # list of weights beta_t for t=0,...,T-1
-		return
+    def __init__(self, clfs: Set[Classifier], T=0):
+        self.clfs = clfs      # set of weak classifiers to be considered
+        self.num_clf = len(clfs)
+        if T < 1:
+            self.T = self.num_clf
+        else:
+            self.T = T
+    
+        self.clfs_picked = [] # list of classifiers h_t for t=0,...,T-1
+        self.betas = []       # list of weights beta_t for t=0,...,T-1
+        return
 
-	@abstractmethod
-	def train(self, features: List[List[float]], labels: List[int]):
-		return
+    @abstractmethod
+    def train(self, features: List[List[float]], labels: List[int]):
+        return
 
-	def predict(self, features: List[List[float]]) -> List[int]:
-		'''
-		Inputs:
-		- features: the features of all test examples
+    def predict(self, features: List[List[float]]) -> List[int]:
+        '''
+        Inputs:
+        - features: the features of all test examples
    
-		Returns:
-		- the prediction (-1 or +1) for each example (in a list)
-		'''
-		########################################################
-		# TODO: implement "predict"
-		########################################################
-		
+        Returns:
+        - the prediction (-1 or +1) for each example (in a list)
+        '''
+        ########################################################
+        # TODO: implement "predict"
+        ########################################################
+        
+        bh = np.zeros((self.T, len(features)))
+        #for t in range(self.T):
+        #    bh[t,:] = self.betas[t] * np.array(self.clfs_picked[t].predict(features)) ##clfs argv
+        t = 0
+        for clfset in self.clfs_picked:
+            bh[t,:] = self.betas[t] * np.array(clfset.predict(features))
+            t += 1
+        
+        predict = (np.sum(bh,axis=0)).astype(int).tolist()
+        
+        return predict
+        
 
 class AdaBoost(Boosting):
-	def __init__(self, clfs: Set[Classifier], T=0):
-		Boosting.__init__(self, clfs, T)
-		self.clf_name = "AdaBoost"
-		return
-		
-	def train(self, features: List[List[float]], labels: List[int]):
-		'''
-		Inputs:
-		- features: the features of all examples
-		- labels: the label of all examples
+    def __init__(self, clfs: Set[Classifier], T=0):
+        Boosting.__init__(self, clfs, T)
+        self.clf_name = "AdaBoost"
+        return
+        
+    def train(self, features: List[List[float]], labels: List[int]):
+        '''
+        Inputs:
+        - features: the features of all examples
+        - labels: the label of all examples
    
-		Require:
-		- store what you learn in self.clfs_picked and self.betas
-		'''
-		############################################################
-		# TODO: implement "train"
-		############################################################
-		
-		
-	def predict(self, features: List[List[float]]) -> List[int]:
-		return Boosting.predict(self, features)
+        Require:
+        - store what you learn in self.clfs_picked and self.betas
+        '''
+        ############################################################
+        # TODO: implement "train"
+        ############################################################
+        N = len(features)
+        D = np.zeros(self.T + 1, N)
+        D[0,:] = 1/N
+        for t in range(self.T):
+            self.clfs_picked.extend(clfs
+        
+        
+        
+        
+    def predict(self, features: List[List[float]]) -> List[int]:
+        return Boosting.predict(self, features)
 
 
 
-	
+    
--- P3/decision_stump.py
+++ P3/decision_stump.py
@@ -3,24 +3,38 @@ from typing import List
 from classifier import Classifier
 
 class DecisionStump(Classifier):
-	def __init__(self, s:int, b:float, d:int):
-		self.clf_name = "Decision_stump"
-		self.s = s
-		self.b = b
-		self.d = d
+    def __init__(self, s:int, b:float, d:int):
+        self.clf_name = "Decision_stump"
+        self.s = s
+        self.b = b
+        self.d = d
 
-	def train(self, features: List[List[float]], labels: List[int]):
-		pass
-		
-	def predict(self, features: List[List[float]]) -> List[int]:
-		'''
-		Inputs:
-		- features: the features of all test examples
+    def train(self, features: List[List[float]], labels: List[int]):
+        pass
+        
+    def predict(self, features: List[List[float]]) -> List[int]:
+        '''
+        Inputs:
+        - features: the features of all test examples
    
-		Returns:
-		- the prediction (-1 or +1) for each example (in a list)
-		'''
-		##################################################
-		# TODO: implement "predict"
-		##################################################
-		
+        Returns:
+        - the prediction (-1 or +1) for each example (in a list)
+        '''
+        ##################################################
+        # TODO: implement "predict"
+        ##################################################
+        
+        #h = np.zeros(len(features))
+        #for num in range(len(features)):
+        #    if features[num][d] > b:
+        #        h[num] = s
+        #    else:
+        #        h[num] = -s
+        #predict = h.tolist()
+        hbool = np.array(features)[:,self.d] > self.b
+        predict = ((hbool * 2 - 1) * self.s).tolist()
+        
+        
+        return predict
+        
+        
